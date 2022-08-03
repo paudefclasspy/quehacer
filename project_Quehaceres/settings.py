d7418c4fa476d251cd2b467926c6263116eef737
paudefclasspy <pauaranega20@gmail.com>
2022-08-04 01:05:06 +0200

    settings change

--- project_Quehaceres/settings.py
+++ project_Quehaceres/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f7a1gqptgblpxsrm-!6_o=no%x)h-o7@zlv-di7_n%c3x250gv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
